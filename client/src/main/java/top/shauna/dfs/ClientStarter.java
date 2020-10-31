9ad23a49b60b57ff9fe69cad4f5410a3df16042a
ShaunaChow <57432193+ShaunaChow@users.noreply.github.com>
2020-10-31 16:12:48 +0800

    '项目部署调试5sss'

--- client/src/main/java/top/shauna/dfs/ClientStarter.java
+++ client/src/main/java/top/shauna/dfs/ClientStarter.java
@@ -1,9 +1,59 @@
 package top.shauna.dfs;
 
+import top.shauna.dfs.service.ClientService;
+import top.shauna.dfs.service.impl.ClientServiceImpl;
+import top.shauna.rpc.bean.FoundBean;
+import top.shauna.rpc.bean.RegisterBean;
+import top.shauna.rpc.config.PubConfig;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @Author Shauna.Chou
  * @Date 2020/10/11 21:52
  * @E-Mail z1023778132@icloud.com
  */
 public class ClientStarter {
+    public static void main(String[] args) throws Exception {
+        preparePubConfig();
+
+        FileChannel fileChannel = new RandomAccessFile(args[0],
+                "rw").getChannel();
+
+        ClientService clientService = new ClientServiceImpl();
+
+        clientService.mkdir("/shauna/");
+
+        clientService.uploadFile("/shauna/1.txt", fileChannel);
+
+        ByteBuffer byteBuffer = clientService.downloadFile("/shauna/1.txt");
+
+        FileChannel fileChannel2 = new RandomAccessFile(args[1],
+                "rw").getChannel();
+
+        fileChannel2.write(byteBuffer);
+
+        fileChannel.close();
+        fileChannel2.close();
+    }
+
+    private static void preparePubConfig() {
+        PubConfig pubConfig = PubConfig.getInstance();
+        pubConfig.setTimeout(100000L);
+        if (pubConfig.getRegisterBean() == null) {
+            RegisterBean registerBean = new RegisterBean("zookeeper", "39.105.89.185:2181", null);
+            pubConfig.setRegisterBean(registerBean);
+        }
+        if (pubConfig.getFoundBean() == null) {
+            RegisterBean registerBean = pubConfig.getRegisterBean();
+            FoundBean foundBean = new FoundBean(
+                    registerBean.getPotocol(),
+                    registerBean.getUrl(),
+                    registerBean.getLoc()
+            );
+            pubConfig.setFoundBean(foundBean);
+        }
+    }
 }
